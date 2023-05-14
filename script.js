7247c547b2325a0c110cfd1c10e82bdba0da51db
Saurav Meghwal <skbmasale941@gmail.com>
2023-05-14 10:41:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,13 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 //old school way
 const request = new XMLHttpRequest();
-request.open('GET');
+request.open('GET', 'https://restcountries.com/v3.1/all');
+request.send();
+
+request.addEventListener('load', function () {
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+
+  //   document.body.innerHTML = data;
+  data.forEach(e => console.log(e.name.common, e.currencies));
+});
