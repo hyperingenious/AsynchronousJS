ac2d47de14bde037194b18a48257d1b046c3e5d0
Saurav Meghwal <skbmasale941@gmail.com>
2023-05-15 03:16:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,66 +5,68 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 //old school way
-const renderCountry = function (data, className = '') {
-  const html = `
-    <article draggable="true" class="country ${className}">
-          <img class="country__img" src="${data.flags.svg}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${data.population}</p>
-            <p class="country__row"><span>🗣️</span> ${data.languages.hin}</p>
-            <p class="country__row"><span>💰</span> ${data.currencies}</p>
-          </div>
-        </article>
-  `;
+// const renderCountry = function (data, className = '') {
+//   const html = `
+//     <article draggable="true" class="country ${className}">
+//           <img class="country__img" src="${data.flags.svg}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${data.population}</p>
+//             <p class="country__row"><span>🗣️</span> ${data.languages.hin}</p>
+//             <p class="country__row"><span>💰</span> ${data.currencies}</p>
+//           </div>
+//         </article>
+//   `;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+// };
 
-const helper = function (url, errorMsg) {
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(`Country not found ${response.status}`);
-    }
-    return response.json();
-  });
-};
+// const helper = function (url, errorMsg) {
+//   return fetch(url).then(response => {
+//     if (!response.ok) {
+//       throw new Error(`Country not found ${response.status}`);
+//     }
+//     return response.json();
+//   });
+// };
 
-const renderError = function (e) {
-  btn.insertAdjacentText(
-    'beforebegin',
-    `${e} there is a error occured from our side`
-  );
-  console.log(e);
-};
+// const renderError = function (e) {
+//   btn.insertAdjacentText(
+//     'beforebegin',
+//     `${e} there is a error occured from our side`
+//   );
+//   console.log(e);
+// };
 
-const getCountries = function (name) {
-  fetch(`https://restcountries.com/v3.1/name/${name}`)
-    .then(response => {
-      if (!response.ok) throw new Error(`Country not found ${response.status}`);
+// const getCountries = function (name) {
+//   fetch(`https://restcountries.com/v3.1/name/${name}`)
+//     .then(response => {
+//       if (!response.ok) throw new Error(`Country not found ${response.status}`);
 
-      return response.json();
-    })
-    .then(data => {
-      const [nData] = data;
-      renderCountry(nData);
+//       return response.json();
+//     })
+//     .then(data => {
+//       const [nData] = data;
+//       renderCountry(nData);
 
-      if (!nData.borders) throw new Error(`no neighbour exists`);
+//       if (!nData.borders) throw new Error(`no neighbour exists`);
 
-      const neighbour = nData.borders[0];
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
-    })
-    .then(response => {
-      if (!response.ok) throw new Error(`Country not found ${response.status}`);
+//       const neighbour = nData.borders[0];
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then(response => {
+//       if (!response.ok) throw new Error(`Country not found ${response.status}`);
 
-      return response.json();
-    })
-    .then(data => {
-      const [nData] = data;
-      renderCountry(nData, 'neighbour');
-    })
-    .catch(e => console.log(e));
-};
-getCountries('pakistan');
+//       return response.json();
+//     })
+//     .then(data => {
+//       const [nData] = data;
+//       renderCountry(nData, 'neighbour');
+//     })
+//     .catch(e => console.log(e));
+// };
+// getCountries('pakistan');
+
+fetch(`https://geocode.xyz/51.50354,-0.12768?geoit=xml&auth=`);
