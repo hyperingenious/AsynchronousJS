84c87951afe24c20390faac6fb423f479a331e88
Saurav Meghwal <skbmasale941@gmail.com>
2023-05-14 17:28:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ const renderCountry = function (data, className = '') {
     <article draggable="true" class="country ${className}">
           <img class="country__img" src="${data.flags.svg}" />
           <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${data.population}</p>
-            <p class="country__row"><span>🗣️</span> ${data.languages.hin}</p>
-            <p class="country__row"><span>💰</span> ${data.currencies}</p>
-          </div>
-        </article>
-  `;
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${data.population}</p>
+//             <p class="country__row"><span>🗣️</span> ${data.languages.hin}</p>
+//             <p class="country__row"><span>💰</span> ${data.currencies}</p>
+//           </div>
+//         </article>
+//   `;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
+  //   countriesContainer.insertAdjacentHTML('beforeend', html);
+  //   countriesContainer.style.opacity = 1;
 };
 /*
 const getCountries = function (country) {
