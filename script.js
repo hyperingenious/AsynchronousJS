8a27980e652c1d53343b03b53f98e4766e80cb3d
Saurav Meghwal <skbmasale941@gmail.com>
2023-05-14 09:56:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+//old school way
+const request = new XMLHttpRequest();
+request.open('GET');
