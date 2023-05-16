9b4376aff55753655e7a8fa06b5ef78695e0b05a
Saurav Meghwal <skbmasale941@gmail.com>
2023-05-16 16:55:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -146,3 +146,14 @@
 
 // whereBtn.addEventListener('click', whereAmI);
 // coordsBtn.addEventListener('click', searchWithCoords);
+
+console.log('Test start');
+document.addEventListener('keydown', () => document.write(`Hello`));
+Promise.resolve('Resolved promise 1').then(res => {
+  console.log(res);
+});
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+console.log('Test end');
